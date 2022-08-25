1743fa83c83ce4d291aefd31731c1a06dab387db
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-08-25 20:08:16 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+namespace QueuePracticeProblem
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Queue <string> queue = new Queue<string>();
+            queue.Enqueue("56");
+            queue.Enqueue("30");
+            queue.Enqueue("70");
+
+            foreach (string ele in queue)
+            {
+                Console.WriteLine(ele);
+
+            }
+            Console.WriteLine("The Peek element is " + queue.Peek());
+
+            Console.WriteLine("-------------------");
+
+            queue.Dequeue();
+
+            foreach(string ele in queue)
+            {
+                Console.WriteLine(ele);
+            }
+            Console.WriteLine("The Peek element is " + queue.Peek());
+
+            Console.WriteLine("-------------------");
+        }
+    }
+}
